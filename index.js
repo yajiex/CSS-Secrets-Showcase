3097b75a945f8306150d47d8c1f338f1ce5be6e2
Yajiex Github <yajiex@outlook.com>
2016-07-27 10:16:08 +0800

    Adjust framework, enable templateSrc tree

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 (function () {
   const slides = document.querySelector('.reveal > .slides');
 
-  const appendLink = (name) => {
+  const initializeReveal = () => {
+    // More info https://github.com/hakimel/reveal.js#configuration
+    Reveal.initialize({
+      history: true,
+
+      // More info https://github.com/hakimel/reveal.js#dependencies
+      dependencies: [
+        { src: 'vendors/reveal.js-3.3.0/plugin/markdown/marked.js' },
+        { src: 'vendors/reveal.js-3.3.0/plugin/markdown/markdown.js' },
+        { src: 'vendors/reveal.js-3.3.0/plugin/notes/notes.js', async: true },
+        {
+          src: 'vendors/reveal.js-3.3.0/plugin/highlight/highlight.js', async: true, callback: function () {
+          hljs.initHighlightingOnLoad();
+        }
+        }
+      ]
+    });
+  };
+
+  const injectTemplate = (section, templateContent) => {
     const updateShadow = (shadow, style, element) => {
       shadow.innerHTML = style + element;
     };
 
     const createCode = (parent, codeText) => {
       const pre = document.createElement('pre');
-      pre.className += ' stretch';
+      pre.classList.add('stretch');
       parent.appendChild(pre);
       const code = document.createElement('code');
-      code.className += ' hljs';
+      code.classList.add('hljs');
       code.setAttribute('contenteditable', 'true');
       code.setAttribute('data-trim', 'true');
       code.innerText = codeText;
@@ -19,35 +38,81 @@
       return code;
     };
 
+    const createNote = (parent, noteText) => {
+      const p = document.createElement('p');
+      p.innerText = noteText;
+      parent.appendChild(p);
+      return p;
+    };
+
+    const createLeftPart = (parent) => {
+      const div = document.createElement('div');
+      div.classList.add('left');
+      parent.appendChild(div);
+      return div;
+    };
+
+    const createRightPart = (parent) => {
+      const div = document.createElement('div');
+      div.classList.add('right');
+      parent.appendChild(div);
+      return div;
+    };
+
+    const left = createLeftPart(section);
+    const right = createRightPart(section);
+    const shadow = right.createShadowRoot();
+
+    createNote(left, templateContent.querySelector('x-notes').innerText.trim());
+    const code = createCode(left, templateContent.querySelector('style').innerHTML.trim());
+    code.addEventListener('input', () => {
+      updateShadow(shadow, '<style>' + code.innerText + '</style>', templateContent.querySelector('x-element').innerHTML);
+    });
+
+    updateShadow(shadow, templateContent.querySelector('style').outerHTML, templateContent.querySelector('x-element').innerHTML);
+  };
+
+  const appendLink = (templatePath, callback) => {
     const link = document.createElement('link');
     link.rel = 'import';
-    link.href = 'src/' + name + '.html';
+    link.href = templatePath;
     link.onload = () => {
-      const section = document.createElement('section');
-      section.className += ' showcase future';
-      const template = link.import.querySelector('template').content;
-      const result = document.createElement('div');
-      const shadow = result.createShadowRoot();
-      const div = document.createElement('div');
-
-      const p = document.createElement('p');
-      p.innerText = template.querySelector('x-notes').innerText.trim();
-      div.appendChild(p);
-      const code = createCode(div, template.querySelector('style').innerHTML.trim());
-      code.addEventListener('input', () => {
-        updateShadow(shadow, '<style>' + code.innerText + '</style>', template.querySelector('x-element').innerHTML);
-      });
-
-      section.appendChild(div);
-      section.appendChild(result);
-
-      updateShadow(shadow, template.querySelector('style').outerHTML, template.querySelector('x-element').innerHTML);
-      slides.appendChild(section);
+      callback(link.import.querySelector('template').content);
     };
     document.head.appendChild(link);
   };
 
-  Reveal.addEventListener('ready', () => {
-    appendLink('bounce');
-  });
+  const init = () => {
+    const templatesSrc = {
+      'Transitions & Animations': {
+        'Elastic transitions': 'bounce.html',
+        'Frame-by-frame animations': 'frame-by-frame.html',
+      },
+    };
+
+    const createSection = (parent) => {
+      const section = document.createElement('section');
+      parent.appendChild(section);
+      return section;
+    };
+
+    Object.keys(templatesSrc).forEach((partKey) => {
+      // Transitions & Animations
+      const partRootSection = createSection(slides);
+      const partSection = createSection(partRootSection);
+      partSection.innerText = partKey;
+      const partVal = templatesSrc[partKey];
+      Object.keys(partVal).forEach((chapterKey) => {
+        // Elastic transitions - bounce.html
+        const chapterSection = createSection(partRootSection);
+        const chapterVal = partVal[chapterKey];
+        appendLink(`src/${partKey}/${chapterKey}/${chapterVal}`, (templateContent) => {
+          injectTemplate(chapterSection, templateContent);
+        });
+      });
+    });
+  };
+
+  init();
+  initializeReveal();
 })();
